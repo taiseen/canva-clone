f425857e33fab52c46a276f34134d3aa64cbaa99
taiseen <taiseen.cse@gmail.com>
2025-04-04 17:42:29 +0600

    feat: project init ✨

---
+++ README.md
@@ -0,0 +1,5 @@
+> 04 - April - 2025
+
+# Canva Clone
+
+- npx create-next-app@15.2.2
---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+const Home = () => {
+  return (
+    <div className="text-3xl text-orange-500">Canva Clone</div>
+  );
+}
+
+export default Home;
